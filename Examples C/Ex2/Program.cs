2d6c897f209d52b85af8434f511f9576200ca325
Alexey Sadchikov <sadaleksej@gmailcom>
2023-06-09 18:59:24 +0300

    Initial

---
+++ Examples C/Ex2/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Clear();
+double Max;
+Console.Write("Введите A: ");
+double A = Double.Parse(Console.ReadLine());
+Console.Write("Введите B: ");
+double B = Double.Parse(Console.ReadLine());
+Console.Write("Введите C: ");
+double C = Double.Parse(Console.ReadLine());
+if(A>=B) Max=A; 
+else Max=B;
+if(C>Max) Max=C;
+Console.WriteLine($"Наибольшее число: {Max}");
