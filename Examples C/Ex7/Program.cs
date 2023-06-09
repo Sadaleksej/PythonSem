437b8ab875c740e02e1f16b8a4e3573a630a31ee
Alexey Sadchikov <sadaleksej@gmailcom>
2023-06-09 19:06:51 +0300

    Ex2

---
+++ Examples C/Ex7/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Clear();
+double C;
+Console.Write("Введите трехзначное число N: ");
+double N = Double.Parse(Console.ReadLine());
+if(N<100 || N>999)
+{Console.WriteLine("Число отсутствует, введено не трехзначное число");
+return;}
+C = N% 10;
+    Console.Write($"последняя цифра во введенном числе: {C}");
+    
+
