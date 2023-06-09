48243c1bf54165fdf8dc372654606549f83fad32
Alexey Sadchikov <sadaleksej@gmailcom>
2023-06-09 19:03:24 +0300

    init4

---
+++ Examples C/Ex4/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.Clear();
+Console.Write("Введите N: ");
+int C = 2;
+double N = Double.Parse(Console.ReadLine());
+if(N<2)
+{Console.WriteLine("Числа отсутствуют");
+return;}
+while(C<=N)
+{
+    Console.Write($" {C}");
+    C=C+2;
+    } 
+
+
+
